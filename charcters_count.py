6b47a6b8f2df0b13de066ee46d7b67fa2541b57f
subha bhatta <153294370+subha-bhatta123@users.noreply.github.com>
2024-01-19 20:07:39 +0545

    Add files via upload

---
+++ charcters_count.py
@@ -0,0 +1,5 @@
+#we have taken an input of a paragraphs or sentences to count the characters.
+p=input("ENTER YOUR PARAGRAPHS TO COUNT CHARACERS: ")
+#len function is used to count the characters.
+pa=len(p)
+print(str(pa))
